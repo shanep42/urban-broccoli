8735783a82d3b9d169d8cb65d7c2a6ab13939d56
Shane <shanep42@gmail.com>
2022-08-18 17:57:22 -0500

    Adds local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
-var now = dayjs()
 // TODO: dayjs().format('LL') should give day without time, but can't get that working
 var dayDisplay = $("#currentDay");
 var textAreas = $("textarea")
 var currentHour = dayjs().hour();
+var saveButtons = document.querySelectorAll(".saveBtn")
+console.log(saveButtons)
 
-dayDisplay.text(now)
+function timeUpdate(){
+    dayDisplay.text(dayjs())
+}
 
-console.log(dayjs().hour())
-console.log(textAreas)
-
-
-// Compares index to current hour (index 0 is hour 9)
+// Loads any local storage previously saved to text area
 for (let i = 0; i < textAreas.length; i++){
+    if (localStorage.getItem(i) != undefined){
+        textAreas[i].value = localStorage.getItem(i);
+    }
+// Compares index to current hour (index 0 is hour 9)
     if (i + 9 < currentHour){
         textAreas[i].classList.remove('present');
         textAreas[i].classList.remove('future');
         textAreas[i].classList.add('past');
+        
     } else if (i + 9 === currentHour){
         textAreas[i].classList.remove('past');
         textAreas[i].classList.remove('future');
@@ -26,3 +30,20 @@ for (let i = 0; i < textAreas.length; i++){
         textAreas[i].classList.add('future');
     }
 }
+
+function testFunction () {
+    console.log("Clicked!")
+}
+
+// TODO: Let user save text in the "hour text content" area to local storage
+
+// Event listener for buttons
+// Saves any text content to local storage using the button index as a key
+for (let i = 0; i < saveButtons.length; i++){
+    saveButtons[i].addEventListener('click', function() {
+        localStorage.setItem(i, textAreas[i].value)
+    })
+}
+
+// Live update the date/time display at top of page (once a second)
+setInterval(timeUpdate, 1000)
